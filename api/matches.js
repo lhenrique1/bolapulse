fafa96d2c2f3b3b5239f68899df76d4935942c4d
lhenrique1 <l.h.o.strik@gmail.com>
2026-04-11 16:43:16 -0300

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -6,15 +6,7 @@ export default async function handler(request, response) {
     const res = await fetch(url, {
       headers: { 'X-Auth-Token': API_KEY }
     });
-    
-    if (!res.ok) {
-        return response.status(res.status).json({ error: "Erro na API externa" });
-    }
-
     const data = await res.json();
-    
-    // Adicionando cabeçalhos para evitar erros de cache
-    response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
     return response.status(200).json(data);
   } catch (e) {
     return response.status(500).json({ error: e.message });
